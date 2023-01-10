427f406499bc4336f057de9c06e3291e2b9a3aa0
a2-toumia <anas2.toumia@live.uwe.ac.uk>
2023-01-10 14:00:24 +0000

    added henrys changes

--- src/examples/my.c
+++ src/examples/my.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <syscall.h>
+#include "userprog/syscall.h"
+#include "lib/stdio.h"
+#include <syscall-nr.h>
+#include "threads/interrupt.h"
 
-int main (void)
+int main(void)
 {
-    //printf("Hello, World\n");
-    create("text.txt", 100);
-    remove("text.txt");
-    exec("hello again!!");
+    printf("Test 1\n");
+    create("text.txt", 0);
+    printf("Test 2\n");
+    remove("test.txt");
+    printf("Test 3\n");
+    halt();
+    //exit(0);
+    //remove("text.txt");
+    //exec("hello again!!");
     return 0;
 }
+
